086c6902ad431e16aa0f760189504e70ec0cc051
Prasad Thete <c-prasad.thete@timesgroup.com>
2024-05-17 14:35:09 +0530

    new

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require('cors');
+const mongoose = require("mongoose");
+const port = 3001;
+const routes = require("./routes");
+
+main().catch((err) => console.log(err));
+
+async function main() {
+  await mongoose.connect("mongodb://mongo:27017/todos", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use("/api", routes);
+  app.get('/', (req, res) => {
+    res.status(200).send('Welcome to the home page!');
+});
+
+  app.listen(port, () => {
+    console.log(`Server is listening on port: ${port}`);
+  });
+}
